0562917db65f6deb45335904abe49f55af4f0701
Endani Munyai <endanimunyai@gmail.com>
2024-03-06 18:02:08 +0200

    Initial Commit

---
+++ Thesis xcode try/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Thesis xcode try
+//
+//  Created by Endani Munyai on 2024. 03. 06..
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
